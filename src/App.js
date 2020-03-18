06ed6593a437e1005ea907f5457b1195b985dec9
Jesus Ponce <j.emmanuel.pb@hotmail.com>
2020-03-18 11:25:22 -0600

    name componente reorganized

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import 'antd/dist/antd.css';
 import { BrowserRouter as Router } from 'react-router-dom';
-import Routing from './components/layout'
+import Routing from './layout/layout'
 
 function App() {
   return (
