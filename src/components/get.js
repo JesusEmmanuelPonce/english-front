b4d1e1d092e4cb7f4b399786685c1c5226937968
Jesus Ponce <j.emmanuel.pb@hotmail.com>
2020-03-18 11:22:34 -0600

    get request to get view

--- src/components/get.js
+++ src/components/get.js
@@ -1,8 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Get = () => {
+
+    const [getPhrasal, setGetPhrasal] = useState([]);
+
+    useEffect(()=> {
+        const getGetPhrasal = async() =>{
+            const resultado = await axios.get('http://localhost:4000/get-phrases');
+            setGetPhrasal(resultado.data.GetPhrases);
+        }
+        getGetPhrasal();
+    },[])
+
+    console.log(getPhrasal);
+
     return(
-        <h1>Get</h1>
+        
+        <div>
+            {
+                getPhrasal.map(item => (
+                    <>
+                        <p>{item.phrase}</p>
+                        <p>{item.mean_spanish}</p>
+                        <p>{item.mean_french}</p>
+                        <p>{item.example_english}</p>
+                        <p>{item.example_spanish}</p>
+                        <p>{item.example_french}</p>
+                    </>
+                ))
+            }
+        </div>
+        
     )
 }
 
