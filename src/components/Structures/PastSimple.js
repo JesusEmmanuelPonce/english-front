15fcb88e5470109b70d4f4de026bbdb9ddd40c61
Jesus Ponce <j.emmanuel.pb@hotmail.com>
2020-03-18 20:07:06 -0600

    Present and past simple file

---
+++ src/components/Structures/PastSimple.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from '../header';
+
+const PastSiemple = () => {
+    return(
+        <>
+            <Header
+                title="Past simple"
+            />
+            <h1>Pasado</h1>
+        </>
+    )
+}
+
+export default PastSiemple;
---
+++ src/components/Structures/PresentSimple.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../header';
+const PresentSimple = () => {
+    return(
+        <>
+            <Header
+             title="Present Simple"
+            />
+            <h1>Presente</h1>
+        </>
+    )
+}
+
+export default PresentSimple;
