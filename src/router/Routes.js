ce64c858e4c65b8f9740dd18eba13c3c2742a454
Jesus Ponce <j.emmanuel.pb@hotmail.com>
2020-03-18 20:08:42 -0600

    present and past simple routes added

--- src/router/Routes.js
+++ src/router/Routes.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Switch as Router, Route } from 'react-router-dom';
 import Get from '../components/get';
+import PresentSimple from '../components/Structures/PresentSimple';
+import PastSimple from '../components/Structures/PastSimple';
 
 const Routes = () => {
     return(
         <Router>
             <Route exact path="/get" component={Get} />
-            {/* <Route exact path="/verbs" component={Verbs}/>
-            <Route exact path="/numbers" component={Numbers}/> */}
+            <Route exact path="/present-simple" component={PresentSimple}/>
+            <Route exact path="/past-simple" component={PastSimple}/>
         </Router>
     )
 }
