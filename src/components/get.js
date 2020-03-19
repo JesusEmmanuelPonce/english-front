e0cbd1393083ecdeed1b4b17ecd45992eb66ac1c
Jesus Ponce <j.emmanuel.pb@hotmail.com>
2020-03-18 20:12:06 -0600

    lines removed

--- src/components/get.js
+++ src/components/get.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { Card } from 'antd';
+import 'antd/dist/antd.css';
 
 const Get = () => {
 
@@ -17,21 +19,20 @@ const Get = () => {
 
     return(
         
-        <div>
-            {
-                getPhrasal.map(item => (
-                    <>
-                        <p>{item.phrase}</p>
+        <div className="container-phrasal-pb">
+            { getPhrasal.map(item => (
+                <div className="site-card-border-less-wrapper">
+                    <Card title={item.phrase} bordered={false} >
                         <p>{item.mean_spanish}</p>
                         <p>{item.mean_french}</p>
                         <p>{item.example_english}</p>
                         <p>{item.example_spanish}</p>
                         <p>{item.example_french}</p>
-                    </>
+                    </Card>
+                </div>
                 ))
             }
         </div>
-        
     )
 }
 
