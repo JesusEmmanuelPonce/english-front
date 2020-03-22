4d822994cf8d260e08cdcc8d725c6c58d29cba02
Jesus Ponce <j.emmanuel.pb@hotmail.com>
2020-03-21 22:24:24 -0600

    table phrasal verbs added to the get component

--- src/components/PhrasalVerbs/Get.js
+++ src/components/PhrasalVerbs/Get.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Card } from 'antd';
-import 'antd/dist/antd.css';
 import './phrasalVerbs.css';
+
+import TablePharasalVerbs from './TablePhrasalVerbs';
 
 const Get = () => {
 
@@ -19,22 +19,9 @@ const Get = () => {
     // console.log(getPhrasal);
 
     return(
-        
-        <div className="container-phrasal-pb">
-            { getPhrasal.map(item => (
-                <div className="site-card-border-less-wrapper card-pb">
-                    <Card title={item.phrase} bordered={false} >
-                        <span className="label-card-pb">Significado</span>
-                        <p>{item.mean_spanish}</p>
-                        <span className="label-card-pb">Ejemplo en ingles</span>
-                        <p>{item.example_english}</p>
-                        <span className="label-card-pb">Ejemplo en español</span>
-                        <p>{item.example_spanish}</p>
-                    </Card>
-                </div>
-                ))
-            }
-        </div>
+        <TablePharasalVerbs 
+            table={getPhrasal}
+        />
     )
 }
 
