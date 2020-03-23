5f1909a21486d19e4d4c14ca9b952bb28f452b2b
Jesus Ponce <j.emmanuel.pb@hotmail.com>
2020-03-22 23:06:45 -0600

    menu json to phrasal verbs

---
+++ src/constant/MENU_PHRASAL_VERBS.js
@@ -0,0 +1,12 @@
+const MENU_PHRASAL_VERBS = [
+    {
+        "name"  : "GET",
+        "route" : "/get"
+    },
+    {
+        "name"  : "PUT",
+        "route" : "/put"
+    },
+];
+
+export default MENU_PHRASAL_VERBS;
