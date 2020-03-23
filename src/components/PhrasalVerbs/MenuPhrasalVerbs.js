2af876449eb9f0bf7b52c823e07fcc588b386b56
Jesus Ponce <j.emmanuel.pb@hotmail.com>
2020-03-22 22:23:42 -0600

    Menu phrasal verbs component

---
+++ src/components/PhrasalVerbs/MenuPhrasalVerbs.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+const MenuPhrasalVerbs = () => {
+    return(
+        <>
+            <Link to="/get">
+                <div className="squad">
+                    <span>Get</span>
+                </div>
+            </Link>
+        </>
+    )
+}
+
+export default MenuPhrasalVerbs;
