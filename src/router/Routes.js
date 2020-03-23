ff7f3f325e98ea5d0fa51b1af3373e64a21f15ef
Jesus Ponce <j.emmanuel.pb@hotmail.com>
2020-03-22 22:26:02 -0600

    Menu phrasal verbs added to the router

--- src/router/Routes.js
+++ src/router/Routes.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Switch as Router, Route } from 'react-router-dom';
+import MenuPhrasalVerbs from '../components/PhrasalVerbs/MenuPhrasalVerbs';
 import Get from '../components/PhrasalVerbs/Get';
 import PresentSimple from '../components/Structures/PresentSimple';
 import PastSimple from '../components/Structures/PastSimple';
@@ -9,6 +10,7 @@ import IrregularVerbs from '../components/Verbs/irregularVerbs';
 const Routes = () => {
     return(
         <Router>
+            <Route exact path="/menu-phrasal-verbs" component={MenuPhrasalVerbs}/>
             <Route exact path="/get" component={Get} />
             <Route exact path="/present-simple" component={PresentSimple}/>
             <Route exact path="/past-simple" component={PastSimple}/>
