4fbda07d3b98dfa68155cd996fdd5835f458c1e6
Jesus Ponce <j.emmanuel.pb@hotmail.com>
2020-03-24 15:15:14 -0600

    menu phrasal verbs json modified

--- src/constant/MENU_PHRASAL_VERBS.js
+++ src/constant/MENU_PHRASAL_VERBS.js
@@ -1,12 +1,35 @@
 const MENU_PHRASAL_VERBS = [
     {
         "name"  : "GET",
-        "route" : "/get"
+        "route" : "/get",
+        "base"  : "square-menu-pb",
+        "color" : "color-blue-pb"
     },
     {
         "name"  : "PUT",
-        "route" : "/put"
+        "route" : "/put",
+        "base"  : "square-menu-pb",
+        "color" : "color-red-pb"
+
     },
+    {
+        "name"  : "COME",
+        "route" : "/come",
+        "base"  : "square-menu-pb",
+        "color" : "color-blue"
+    },
+    {
+        "name"  : "LOOK",
+        "route" : "/look",
+        "base"  : "square-menu-pb",
+        "color" : "color-blue"
+    },
+    {
+        "name"  : "TAKE",
+        "route" : "/take",
+        "base"  : "square-menu-pb",
+        "color" : "color-blue"
+    }
 ];
 
 export default MENU_PHRASAL_VERBS;
