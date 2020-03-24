8d043206729767338f305351271b66fd736ff1d2
Jesus Ponce <j.emmanuel.pb@hotmail.com>
2020-03-24 15:13:04 -0600

    Menu verbs and structures added

--- src/router/Routes.js
+++ src/router/Routes.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Switch as Router, Route } from 'react-router-dom';
 import MenuPhrasalVerbs from '../components/PhrasalVerbs/MenuPhrasalVerbs';
+import MenuVerbs from '../components/Verbs/MenuVerbs';
+import MenuStructures from '../components/Structures/MenuStructures';
 import Get from '../components/PhrasalVerbs/Get';
 import PresentSimple from '../components/Structures/PresentSimple';
 import PastSimple from '../components/Structures/PastSimple';
@@ -11,6 +13,8 @@ const Routes = () => {
     return(
         <Router>
             <Route exact path="/menu-phrasal-verbs" component={MenuPhrasalVerbs}/>
+            <Route exact path="/menu-verbs" component={MenuVerbs}/>
+            <Route exact path="/menu-estructuras" component={MenuStructures}/>
             <Route exact path="/get" component={Get} />
             <Route exact path="/present-simple" component={PresentSimple}/>
             <Route exact path="/past-simple" component={PastSimple}/>
