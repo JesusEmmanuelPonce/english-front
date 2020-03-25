a9bba7b1414f8a157a381e23ae1722d4c2a8f8cf
Jesus Ponce <j.emmanuel.pb@hotmail.com>
2020-03-24 23:22:06 -0600

    menu verbs file

---
+++ src/constant/MENU_VERBS.js
@@ -0,0 +1,12 @@
+const MENU_VERBS = [
+    {
+        "name" : "Verbos regulares",
+        "route" : "/regular-verbs"
+    },
+    {
+        "name" : "Verbos irregulares",
+        "route" : "/irregular-verbs"
+    }
+];
+
+export default MENU_VERBS;
