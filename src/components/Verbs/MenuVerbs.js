e77785c363c223959c6c83dfa0c9b3d650ee3a83
Jesus Ponce <j.emmanuel.pb@hotmail.com>
2020-03-24 18:12:24 -0600

    Containers changed

--- src/components/Verbs/MenuVerbs.js
+++ src/components/Verbs/MenuVerbs.js
@@ -1,8 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './verbs.css';
 
 const MenuVerbs = () => {
     return(
-        <h1>Verbos</h1>
+        <div className="container-verbs-pb">
+            <div>
+                <Link to="/regular-verbs">
+                    <div className="container-regular-pb">
+                        Verbos regulares
+                        </div>
+                </Link>
+            </div>
+            <div>
+                <Link to="/irregular-verbs">
+                    <div className="container-irregular-pb">
+                        Verbos irregulares
+                    </div>
+                </Link>
+            </div>
+        </div> 
     )
 }
 
