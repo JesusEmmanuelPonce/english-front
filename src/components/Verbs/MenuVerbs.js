3682931626833e54312a2cfdc6081df0b058b4cd
Jesus Ponce <j.emmanuel.pb@hotmail.com>
2020-03-24 23:24:44 -0600

    add styles to the verbs

--- src/components/Verbs/MenuVerbs.js
+++ src/components/Verbs/MenuVerbs.js
@@ -1,24 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './verbs.css';
+import MENU_VERBS from '../../constant/MENU_VERBS';
 
 const MenuVerbs = () => {
     return(
         <div className="container-verbs-pb">
-            <div>
-                <Link to="/regular-verbs">
-                    <div className="container-regular-pb">
-                        Verbos regulares
-                        </div>
-                </Link>
-            </div>
-            <div>
-                <Link to="/irregular-verbs">
-                    <div className="container-irregular-pb">
-                        Verbos irregulares
+           {
+               MENU_VERBS.map(item => (
+                    <div className="square-verbs-pb" key={item.name}>
+                        <Link to={item.route}>
+                            <div className="content-verbs-pb">
+                                <span className="letter-verbs">{item.name}</span>
+                            </div>
+                        </Link>
                     </div>
-                </Link>
-            </div>
+               ))
+           }
         </div> 
     )
 }
