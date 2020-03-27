156ca73b7d024b50bfd955b4dcb241443ec3360e
Jesus Ponce <j.emmanuel.pb@hotmail.com>
2020-03-27 11:11:54 -0600

    main component

---
+++ src/components/main.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Main = () => {
+    return(
+        <h1>Selecciones una opcion</h1>
+    )
+}
+
+export default Main;
--- src/router/Routes.js
+++ src/router/Routes.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Switch as Router, Route } from 'react-router-dom';
+import Main from '../components/main';
 import MenuPhrasalVerbs from '../components/PhrasalVerbs/MenuPhrasalVerbs';
 import MenuVerbs from '../components/Verbs/MenuVerbs';
 import MenuStructures from '../components/Structures/MenuStructures';
@@ -12,6 +13,7 @@ import IrregularVerbs from '../components/Verbs/irregularVerbs';
 const Routes = () => {
     return(
         <Router>
+            <Route exact path="/" component={Main}/>
             <Route exact path="/menu-phrasal-verbs" component={MenuPhrasalVerbs}/>
             <Route exact path="/menu-verbs" component={MenuVerbs}/>
             <Route exact path="/menu-estructuras" component={MenuStructures}/>
