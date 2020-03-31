263402810942ca500832a6fe30353cf8bf811f2e
Jesus Ponce <j.emmanuel.pb@hotmail.com>
2020-03-31 13:51:58 -0600

    Http Get to get nationalities

--- src/components/Vocabulary/Nationatities/nationalities.js
+++ src/components/Vocabulary/Nationatities/nationalities.js
@@ -1,8 +1,38 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import Header from '../../header';
+import Loader from '../../../loader/loader';
+import TableNationalities from './TableNationalities';
 
 const Nationalities = () =>{
+
+    const [nationalities, setNationalities] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(()=>{
+        const getNationalities = async() =>{
+            const resultado = await axios.get('https://backend-english-pb.herokuapp.com/nationalities');
+            setNationalities(resultado.data.nationalities);
+            setLoading(false);
+        }
+
+        getNationalities();
+    },[]);
+
+    console.log(nationalities);
+
     return(
-        <h1>Nacionalidades</h1>
+        <>
+            <Header
+                title="Nacionalidades"
+            />
+            {
+                loading ? <Loader loading={loading}/>
+                        : <TableNationalities
+                            table={nationalities}
+                          />
+            }
+        </>
     )
 }
 
