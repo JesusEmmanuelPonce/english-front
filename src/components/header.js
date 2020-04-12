d4d4e3a9e4f40eb3758fbea59c648160c77e0a3b
Jesus Ponce <j.emmanuel.pb@hotmail.com>
2020-04-11 21:17:22 -0500

    history go back button

--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import './header.css';
+import { LeftOutlined } from '@ant-design/icons';
 
-const Header = ({title}) => (
-    <div className="header-container-pb">
-       <span>{title}</span>
-    </div>
-)
+const Header = ({title}) => {
+
+    const history = useHistory()
+
+    return(
+        <div className="header-container-pb">
+            <button className="btn-header-pb" onClick={()=> history.goBack()}><LeftOutlined className="arrow-header-pb"/></button>
+            <span>{title}</span>
+        </div>
+    )
+}
 
 export default Header;
