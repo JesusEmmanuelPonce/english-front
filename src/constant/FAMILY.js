b2bcaead4ab4c5cef878c747fbc34bb2cdae8017
Jesus Ponce <j.emmanuel.pb@hotmail.com>
2020-04-11 21:16:30 -0500

    family json file

---
+++ src/constant/FAMILY.js
@@ -0,0 +1,48 @@
+const FAMILY = [
+    {
+        "member_en" : "Grandfather", 
+        "member_sp" : "Abuelo"
+    },
+    {
+        "member_en" : "Grandmother", 
+        "member_sp" : "Abuela"
+    },
+    {
+        "member_en" : "Grandson", 
+        "member_sp" : "Nieto"
+    },
+    {
+        "member_en" : "Granddaughter", 
+        "member_sp" : "Nieta"
+    },
+    {
+        "member_en" : "Uncle", 
+        "member_sp" : "Tío"
+    },
+    {
+        "member_en" : "Aunt", 
+        "member_sp" : "Tía"
+    },
+    {
+        "member_en" : "Cousin", 
+        "member_sp" : "Primo"
+    },
+    {
+        "member_en" : "Father", 
+        "member_sp" : "Padre"
+    },
+    {
+        "member_en" : "Mother", 
+        "member_sp" : "Madre"
+    },
+    {
+        "member_en" : "Daughter", 
+        "member_sp" : "Hija"
+    },
+    {
+        "member_en" : "Son", 
+        "member_sp" : "Hijo"
+    }
+]
+
+export default FAMILY;
