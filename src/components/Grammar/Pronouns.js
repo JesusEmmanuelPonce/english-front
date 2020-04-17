1076145b55fc28562b70c0dc5314310a75b08f50
Jesus Ponce <j.emmanuel.pb@hotmail.com>
2020-04-17 11:58:08 -0500

    pronouns examples

--- src/components/Grammar/Pronouns.js
+++ src/components/Grammar/Pronouns.js
@@ -1,18 +1,28 @@
 import React from 'react';
 import Header from '../header';
 import PRONOUNS from '../../constant/PRONOUNS';
+import './pronouns.css';
 
 const Pronouns = () => {
     return(
         <>
             <Header title="Pronombres"/>
-                {
-                    PRONOUNS.map(item => (
-                        <div key={item.id}>
-                            <p>{item.pronouns} - {item.mean}</p>
-                        </div>
-                    ))
-                }
+                <div className="container-pronouns-pb">
+                    <div className="pronouns-pb">
+                    {
+                        PRONOUNS.map(item => (
+                                <p key={item.id}>
+                                    <span>{item.pronouns}</span> - <span>{item.mean}</span>
+                                </p>
+                        ))
+                    }
+                    </div>
+                    <div className="example-pb">
+                        <p>Ejemplo</p>
+                        <p><span className="pronouns-personal">I</span> want a new bike for my birthday - <span>Quiero una bicicleta para mi cumpleaños</span></p>
+                        <p><span className="pronouns-personal">You</span> play the piano - <span>Tú tocas el piano</span></p>
+                    </div>
+                </div>
         </>
     )
 }
